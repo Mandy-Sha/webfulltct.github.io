e1fd22140113bc794f02448dab9ef2b5bac0c6d9
zhaofeng <feng.zhao@samsung.com>
2020-07-31 16:32:57 +0800

    add source

---
+++ precache-manifest.b9b4194e1e8b6e1437f3351cad07799e.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef27bc21a39326edbde9a4ad3fb1460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09725ca88dcdd6398b86",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "2904b532ecb858b7f7c7",
+    "url": "/static/css/main.d2d59b8d.chunk.css"
+  },
+  {
+    "revision": "09725ca88dcdd6398b86",
+    "url": "/static/js/2.8286b0cd.chunk.js"
+  },
+  {
+    "revision": "2904b532ecb858b7f7c7",
+    "url": "/static/js/main.6d6e177d.chunk.js"
+  },
+  {
+    "revision": "b78479c4fbcd3ad9856d",
+    "url": "/static/js/runtime-main.4b9304c4.js"
+  },
+  {
+    "revision": "74eca06c75b212f515bb41e8c4143f26",
+    "url": "/static/media/1-3-2.74eca06c.jpg"
+  },
+  {
+    "revision": "0434a345c39b65138af9e044c6b0ba64",
+    "url": "/static/media/1.0434a345.jpg"
+  },
+  {
+    "revision": "a6c9fdeded1f6c2546e4accec1fa3999",
+    "url": "/static/media/1.a6c9fded.jpg"
+  },
+  {
+    "revision": "73f5ff3849c64d54f3269440f49b6f0b",
+    "url": "/static/media/1520406194323961.73f5ff38.jpg"
+  },
+  {
+    "revision": "9d749bc3cf4563ef33753759e7059300",
+    "url": "/static/media/155149.53383062_1000.9d749bc3.jpg"
+  },
+  {
+    "revision": "a787ee2ab0af4c68945533327c737f51",
+    "url": "/static/media/2.a787ee2a.jpg"
+  },
+  {
+    "revision": "aa48c600054bc7cc5bb41c1fb0bca39c",
+    "url": "/static/media/2.aa48c600.jpg"
+  },
+  {
+    "revision": "59d313ebf86811a9f4a7af0290eaa184",
+    "url": "/static/media/3.59d313eb.jpg"
+  },
+  {
+    "revision": "905eaf0f91008b19c99590b7fba9bd69",
+    "url": "/static/media/3.905eaf0f.jpg"
+  },
+  {
+    "revision": "04476f4e468ff4acbb0ed3f3491fde60",
+    "url": "/static/media/4.04476f4e.jpg"
+  },
+  {
+    "revision": "b7c4547b93ad63231b02c025ff1ba12d",
+    "url": "/static/media/4.b7c4547b.jpg"
+  },
+  {
+    "revision": "088532e1590a7edf41b9f584fe0276a8",
+    "url": "/static/media/5.088532e1.jpg"
+  },
+  {
+    "revision": "426d66d80174f6d35fa0b6f0d1737147",
+    "url": "/static/media/5.426d66d8.jpg"
+  },
+  {
+    "revision": "8014034e68872aa0233553b9ba3d94ca",
+    "url": "/static/media/775a-hxsrwwr3696326.8014034e.jpg"
+  },
+  {
+    "revision": "2830362e98caea10f91c0d190e144fc8",
+    "url": "/static/media/Horizontal_bg.2830362e.png"
+  },
+  {
+    "revision": "1cfbee2347cf7b94bac3780630c3c7e1",
+    "url": "/static/media/SamsungOneLatin-400_v1.1.1cfbee23.ttf"
+  },
+  {
+    "revision": "3341ab46b59339945e10410027e4b9dd",
+    "url": "/static/media/SamsungOneLatin-800_v1.1.3341ab46.ttf"
+  },
+  {
+    "revision": "b7cccf9944f7e472b17fe8a9945ced98",
+    "url": "/static/media/SamsungOneUI-400_V3.0.b7cccf99.ttf"
+  },
+  {
+    "revision": "8bde47ae165c5eb53998a07b7ea719ef",
+    "url": "/static/media/SamsungOneUI-500_V3.0.8bde47ae.ttf"
+  },
+  {
+    "revision": "4a79fce53aef941f5d3d0d473af02e59",
+    "url": "/static/media/SamsungOneUI-700_V3.0.4a79fce5.ttf"
+  },
+  {
+    "revision": "7d593d5e277ab256e1efe0ec0b7431cd",
+    "url": "/static/media/Vertical_bg.7d593d5e.png"
+  },
+  {
+    "revision": "3117a60fdac5171e9e36a20c8ba60d77",
+    "url": "/static/media/liX4p5kIMnckQ.3117a60f.jpg"
+  },
+  {
+    "revision": "b218b64e4e23913779e3d471c6a601fa",
+    "url": "/static/media/nav_Concert_focus.b218b64e.png"
+  },
+  {
+    "revision": "b2d6e52ec28889e4449bdd7b35189d2b",
+    "url": "/static/media/nav_Dance_focus.b2d6e52e.png"
+  },
+  {
+    "revision": "ecbee074f78566d48c07ae73a96ef450",
+    "url": "/static/media/nav_ForYou_focus.ecbee074.png"
+  },
+  {
+    "revision": "43c04423802396f22352ca194db672f0",
+    "url": "/static/media/player_play_focused.43c04423.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
