5ac0a115a8f769c2b11d89dff5036276f4bae764
cherry.wu <cherry.wu@samsung.com>
2020-08-27 16:28:20 +0800

    Add new version
    Signed-off-by: cherry.wu <cherry.wu@samsung.com>

---
+++ precache-manifest.c7caad48dea4588730c847d7fd522411.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f8e3af57a90026b9271a40a129f58c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eed868449c1ec62d0176",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "4a9fb15d5bac002462c1",
+    "url": "/static/css/main.759561c1.chunk.css"
+  },
+  {
+    "revision": "eed868449c1ec62d0176",
+    "url": "/static/js/2.827aa1d3.chunk.js"
+  },
+  {
+    "revision": "4a9fb15d5bac002462c1",
+    "url": "/static/js/main.09d5464e.chunk.js"
+  },
+  {
+    "revision": "b78479c4fbcd3ad9856d",
+    "url": "/static/js/runtime-main.4b9304c4.js"
+  },
+  {
+    "revision": "5369994fdb8cd34078fc6752187ff292",
+    "url": "/static/media/1.5369994f.mp4"
+  },
+  {
+    "revision": "6f7cfb44a34d30b960087a398d0b2cc4",
+    "url": "/static/media/1.6f7cfb44.jpg"
+  },
+  {
+    "revision": "952d89ee95be245fc7a178206c5e3376",
+    "url": "/static/media/10.952d89ee.jpg"
+  },
+  {
+    "revision": "89dec5d779569f915bf77a0f01ed462c",
+    "url": "/static/media/11.89dec5d7.jpg"
+  },
+  {
+    "revision": "aad226172d982efae9018aaf072c502c",
+    "url": "/static/media/12.aad22617.jpg"
+  },
+  {
+    "revision": "e7b53be4586bb29cf9c3aaef4e90abde",
+    "url": "/static/media/13.e7b53be4.jpg"
+  },
+  {
+    "revision": "4c4fd86689afb7af511694d52eb4ddae",
+    "url": "/static/media/14.4c4fd866.jpg"
+  },
+  {
+    "revision": "3788eadfa3631c3dece0b6f4352b00ec",
+    "url": "/static/media/15.3788eadf.jpg"
+  },
+  {
+    "revision": "70fbc09ace35b4e17c3774a0b48ea8ee",
+    "url": "/static/media/16.70fbc09a.jpg"
+  },
+  {
+    "revision": "e098723dbea7894c6a3e34146b87efd2",
+    "url": "/static/media/17.e098723d.jpg"
+  },
+  {
+    "revision": "07b32422afeba7f71ba3ace573ba2f25",
+    "url": "/static/media/18.07b32422.jpg"
+  },
+  {
+    "revision": "2c0c3a314ec1634f6da0abf0b49f634e",
+    "url": "/static/media/19.2c0c3a31.jpg"
+  },
+  {
+    "revision": "a3fbd0bad3cb89a5dcf06a3e4e457c98",
+    "url": "/static/media/2.a3fbd0ba.jpg"
+  },
+  {
+    "revision": "d1952ed94feb34d2de4be72287039867",
+    "url": "/static/media/20.d1952ed9.jpg"
+  },
+  {
+    "revision": "1c416d85eba0b13e0f5596f1f7d9f36a",
+    "url": "/static/media/21.1c416d85.jpg"
+  },
+  {
+    "revision": "863a8b6cd3111a7f69eb3da1abfdf905",
+    "url": "/static/media/22.863a8b6c.jpg"
+  },
+  {
+    "revision": "e4e4d2816877d5111f2f7e1fc2d5d3fa",
+    "url": "/static/media/23.e4e4d281.jpg"
+  },
+  {
+    "revision": "58a2ad743b22f956d1ac646816758d29",
+    "url": "/static/media/24.58a2ad74.jpg"
+  },
+  {
+    "revision": "87695ce43532b32a245e86972d067771",
+    "url": "/static/media/25.87695ce4.jpg"
+  },
+  {
+    "revision": "32f9806c0a85a7d5a1907fe2f8a7efc6",
+    "url": "/static/media/26.32f9806c.jpg"
+  },
+  {
+    "revision": "78c31ffa4a603168a80b37d34b2fc5b9",
+    "url": "/static/media/27.78c31ffa.jpg"
+  },
+  {
+    "revision": "5341e00fd98cbfcfa7dfafe7c0efeed6",
+    "url": "/static/media/28.5341e00f.jpg"
+  },
+  {
+    "revision": "0dcf4ba15aeae39f27a3ef14fb36d673",
+    "url": "/static/media/29.0dcf4ba1.jpg"
+  },
+  {
+    "revision": "ce805a46957e2299090ac83f60624dd7",
+    "url": "/static/media/3.ce805a46.jpg"
+  },
+  {
+    "revision": "bc63ce2cdade9bd412aae2a6315b28df",
+    "url": "/static/media/30.bc63ce2c.jpg"
+  },
+  {
+    "revision": "cea51882f309c3b5d36345354ef95db4",
+    "url": "/static/media/31.cea51882.jpg"
+  },
+  {
+    "revision": "02e0d7fb5f526ef581117acafc04bfaf",
+    "url": "/static/media/32.02e0d7fb.jpg"
+  },
+  {
+    "revision": "89b647bcce1131a61856fbe13928580a",
+    "url": "/static/media/33.89b647bc.jpg"
+  },
+  {
+    "revision": "31bfd4d1084fe2da132cf6e158709b2f",
+    "url": "/static/media/34.31bfd4d1.jpg"
+  },
+  {
+    "revision": "d9e356fd175336dd3c435d8eb6a54ffb",
+    "url": "/static/media/35.d9e356fd.jpg"
+  },
+  {
+    "revision": "f6d3fff14ee36bac95e4d83d38fd766d",
+    "url": "/static/media/36.f6d3fff1.jpg"
+  },
+  {
+    "revision": "1d65b540e1d00e32b1a76e9a10727809",
+    "url": "/static/media/37.1d65b540.jpg"
+  },
+  {
+    "revision": "c426454c66add6c8a7ac36222ed9acd4",
+    "url": "/static/media/38.c426454c.jpg"
+  },
+  {
+    "revision": "8f4760b128f9af98c43ed7e7a4388468",
+    "url": "/static/media/39.8f4760b1.jpg"
+  },
+  {
+    "revision": "376629d0d9a8ab918252934959d98dd6",
+    "url": "/static/media/4.376629d0.jpg"
+  },
+  {
+    "revision": "65b0896a6054e1a99b8f77cc55052322",
+    "url": "/static/media/40.65b0896a.jpg"
+  },
+  {
+    "revision": "1e0c41f8171799a6af3ba0b7428a7081",
+    "url": "/static/media/41.1e0c41f8.jpg"
+  },
+  {
+    "revision": "6a994d7c1a7a8cc56282db40ccf8676d",
+    "url": "/static/media/42.6a994d7c.jpg"
+  },
+  {
+    "revision": "5538197c25d30825ab9b01ed0847459c",
+    "url": "/static/media/43.5538197c.jpg"
+  },
+  {
+    "revision": "6a5553c4a4bf9bc5ac7ad4a9d74ab440",
+    "url": "/static/media/44.6a5553c4.jpg"
+  },
+  {
+    "revision": "8d02d299aff38044c16de5af5ad1aa16",
+    "url": "/static/media/45.8d02d299.jpg"
+  },
+  {
+    "revision": "67b8d329eea9d66ef6f43c7846e1f308",
+    "url": "/static/media/46.67b8d329.jpg"
+  },
+  {
+    "revision": "133dc279e1bb8a7bf23e673cadd37ac1",
+    "url": "/static/media/47.133dc279.jpg"
+  },
+  {
+    "revision": "75e246f15f8988e846eeeae1a77b0a0c",
+    "url": "/static/media/48.75e246f1.jpg"
+  },
+  {
+    "revision": "745799707cd86699a0cc9d20598e9640",
+    "url": "/static/media/49.74579970.jpg"
+  },
+  {
+    "revision": "3990915364b92f33ec65ec0759959010",
+    "url": "/static/media/5.39909153.jpg"
+  },
+  {
+    "revision": "d7f1648ca24fcb2b04d82a90bb03a999",
+    "url": "/static/media/50.d7f1648c.jpg"
+  },
+  {
+    "revision": "b7592bfdd0a3f18cb8de790d28c3c8f0",
+    "url": "/static/media/6.b7592bfd.jpg"
+  },
+  {
+    "revision": "d238472f8668ec95fd36ce75469e7c10",
+    "url": "/static/media/7.d238472f.jpg"
+  },
+  {
+    "revision": "d65e8c377a45c059a99a9c458c107937",
+    "url": "/static/media/8.d65e8c37.jpg"
+  },
+  {
+    "revision": "a2054ad80042ac1e0d987e22f13440f1",
+    "url": "/static/media/9.a2054ad8.jpg"
+  },
+  {
+    "revision": "2830362e98caea10f91c0d190e144fc8",
+    "url": "/static/media/Horizontal_bg.2830362e.png"
+  },
+  {
+    "revision": "1cfbee2347cf7b94bac3780630c3c7e1",
+    "url": "/static/media/SamsungOneLatin-400_v1.1.1cfbee23.ttf"
+  },
+  {
+    "revision": "3341ab46b59339945e10410027e4b9dd",
+    "url": "/static/media/SamsungOneLatin-800_v1.1.3341ab46.ttf"
+  },
+  {
+    "revision": "b7cccf9944f7e472b17fe8a9945ced98",
+    "url": "/static/media/SamsungOneUI-400_V3.0.b7cccf99.ttf"
+  },
+  {
+    "revision": "8bde47ae165c5eb53998a07b7ea719ef",
+    "url": "/static/media/SamsungOneUI-500_V3.0.8bde47ae.ttf"
+  },
+  {
+    "revision": "4a79fce53aef941f5d3d0d473af02e59",
+    "url": "/static/media/SamsungOneUI-700_V3.0.4a79fce5.ttf"
+  },
+  {
+    "revision": "7d593d5e277ab256e1efe0ec0b7431cd",
+    "url": "/static/media/Vertical_bg.7d593d5e.png"
+  },
+  {
+    "revision": "b218b64e4e23913779e3d471c6a601fa",
+    "url": "/static/media/nav_Concert_focus.b218b64e.png"
+  },
+  {
+    "revision": "b2d6e52ec28889e4449bdd7b35189d2b",
+    "url": "/static/media/nav_Dance_focus.b2d6e52e.png"
+  },
+  {
+    "revision": "ecbee074f78566d48c07ae73a96ef450",
+    "url": "/static/media/nav_ForYou_focus.ecbee074.png"
+  },
+  {
+    "revision": "43c04423802396f22352ca194db672f0",
+    "url": "/static/media/player_play_focused.43c04423.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
